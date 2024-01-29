b5c5cd180c9419fff063af341a519274a9f8499b
Ananya Rajagopal <apr5@illinois.edu>
2024-01-28 22:05:07 -0600

    push

--- serve.py
+++ serve.py
@@ -3,17 +3,14 @@ import socket
 from flask import Flask, request
 app = Flask(__name__)
 
-
 @app.route('/', methods=['POST', 'GET'])
-def main():
-    global seed
+def handle_request():
     if request.method == 'POST':
-        subprocess.run(["python3", "stress_cpu.py"])
-        return "post"
+        subprocess.Popen(['python3', 'stress_cpu.py'])
+        return 'Stressing CPU initiated\n', 200
     elif request.method == 'GET':
         private_ip = socket.gethostbyname(socket.gethostname())
         return f'Private IP address: {private_ip}\n', 200
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
