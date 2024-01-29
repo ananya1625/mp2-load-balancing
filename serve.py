6f3b692bd37fc15b122525444c325126540176c4
Ananya Rajagopal <apr5@illinois.edu>
2024-01-28 21:46:18 -0600

    push

--- serve.py
+++ serve.py
@@ -9,6 +9,7 @@ def main():
     global seed
     if request.method == 'POST':
         subprocess.run(["python3", "stress_cpu.py"])
+        return "post"
     elif request.method == 'GET':
         private_ip = socket.gethostbyname(socket.gethostname())
         return f'Private IP address: {private_ip}\n', 200
